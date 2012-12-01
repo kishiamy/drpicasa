b5aede97e60f02642cbe280d79424d35132037a8
kishiamy <kishiamy84@gmail.com>
2012-12-01 10:32:42 +0100

    add devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drpicasa::Application.config.secret_token = 'e8cf7d4ba2c8111b84414d862b500902a8cf961627bbd07d3f5622dbfceade5ef11fddb7cdf79ee45c7d0373661df82a0c1113920e9653d7782ec2c149798169'
