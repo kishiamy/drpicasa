d797a6e7f6becd6e3e0e2a645ef99c4f3dc5dcbb
kishiamy <kishiamy84@gmail.com>
2012-12-20 00:55:41 +0000

    delete scope from omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :google_oauth2, '1002583254671.apps.googleusercontent.com', 'ueU7Un8nTThg-rvDSQe5IEXc', :scope => "https://picasaweb.google.com/data/"
+   provider :google_oauth2, '1002583254671.apps.googleusercontent.com', 'ueU7Un8nTThg-rvDSQe5IEXc'
 end
