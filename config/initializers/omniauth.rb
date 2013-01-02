85109e159ebcf34cdd0852573c1fb9ee7d80f297
kishiamy <kishiamy84@gmail.com>
2013-01-02 14:25:45 +0000

    added callback to omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :google_oauth2,ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {access_type: 'online', approval_prompt: '', :scope => 'http://picasaweb.google.com/data/'}
+   provider :google_oauth2,ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {access_type: 'online', approval_prompt: '', :scope => 'http://picasaweb.google.com/data/', :redirect_uri => 'http://drpicasa.herokuapp.com/auth/google_oauth2/callback'}
 end
