57b8706d3f5e51a3c99beb9f91b56b4842ac0820
kishiamy <kishiamy84@gmail.com>
2013-04-21 19:31:40 +0200

    delete some scaffolds

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['348464901716.apps.googleusercontent.com'], ENV['UBnaijhE39wJqVEIeTa4PC7h'], 
+  provider :google_oauth2, ENV['947033028852.apps.googleusercontent.com'], ENV['Czqi1f9U61n19KjJ9OwDyYAc'], 
   { :scope => "userinfo.email,userinfo.profile,https://picasaweb.google.com/data/",
     :access_type => 'online', 
     :approval_prompt => 'auto'
   }
   # redirect_uri = https://localhost:3000/oauth2callback
 end
+
