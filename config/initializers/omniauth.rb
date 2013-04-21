bed4a30e7f1e7b599e12b60520b33d344b5ae752
kishiamy <kishiamy84@gmail.com>
2013-04-21 15:06:46 +0200

    setting routes and omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-   provider :google_oauth2,ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {access_type: 'online', approval_prompt: '', :scope => 'http://picasaweb.google.com/data/', :redirect_uri => 'http://drpicasa.herokuapp.com/auth/google_oauth2/callback'}
+  provider :google_oauth2, ENV['348464901716.apps.googleusercontent.com'], ENV['UBnaijhE39wJqVEIeTa4PC7h'], 
+  { :scope => "userinfo.email,userinfo.profile,https://picasaweb.google.com/data/",
+    :access_type => 'online', 
+    :approval_prompt => 'auto'
+  }
+  # redirect_uri = https://localhost:3000/oauth2callback
 end
