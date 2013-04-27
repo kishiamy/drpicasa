89bfa136e4e72479e0a3b41074e1e7f1445b45b2
kishiamy <kishiamy84@gmail.com>
2013-04-27 22:56:10 +0200

    set omniauth variables in bash_profile

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, CLIENT_ID = '947033028852.apps.googleusercontent.com', CLIENT_SECRET = 'Czqi1f9U61n19KjJ9OwDyYAc', 
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], 
   { :scope => "userinfo.email,userinfo.profile,https://picasaweb.google.com/data/",
     :access_type => 'online', 
     :approval_prompt => 'auto'
