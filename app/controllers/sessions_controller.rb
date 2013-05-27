744a3c99621a8c9f23ccf6d8170a355cc04a806f
kishiamy <kishiamy84@gmail.com>
2013-05-27 23:06:18 +0200

    set case for empty albums

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,13 @@ class SessionsController < ApplicationController
   def albums
     token = request.env["omniauth.auth"].credentials.token
     request_xml = RestClient.get("https://picasaweb.google.com/data/feed/api/user/default", 
-                  headers = { Gdata: 2,
-                              Authorization: "OAuth #{token}" })
+                                  headers = { Gdata: 2,
+                                              Authorization: "OAuth #{token}" })
 
     albums_xml = Nokogiri::XML(request_xml) do |config|
       config.strict.nonet
     end
-    #albums_xml = Nokogiri::XML::Document.parse(request_xml) { |config| config.nonet }
-
+    
     @albums = albums_xml.xpath('//xmlns:entry').map do |entry|
       {
         :thumb => entry.xpath('//media:thumbnail').attr("url"),
