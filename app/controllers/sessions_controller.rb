7abb7262e6049d0b31bfd1e117fb7bf6af3face4
kishiamy <kishiamy84@gmail.com>
2013-05-29 00:03:59 +0200

    create show photos

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
 
+  #TODO: separate get token,get albums and get photos in different methods
   def albums
     token = request.env["omniauth.auth"].credentials.token
     request_xml = RestClient.get("https://picasaweb.google.com/data/feed/api/user/default", 
@@ -10,15 +11,24 @@ class SessionsController < ApplicationController
       config.strict.nonet
     end
     
-    @albums = albums_xml.xpath("//xmlns:entry").map do |entry|
+    @albums = albums_xml.xpath('//xmlns:entry').map do |entry|
       {
-         :title => entry.xpath('.//media:title').inner_text,
-        :thumb => entry.xpath('.//media:thumbnail').attr("url")  
+        :title => entry.xpath('.//media:title').inner_text,
+        :thumb => entry.xpath('.//media:thumbnail').attr("url"),
+      }
+    end
+
+    @photos = albums_xml.xpath('//media:group')[0...3].each do |entry|
+      {
+        :image => entry.xpath('.//media:content').attr("url")
       }
     end
   end
 
-  def logi
+  def photos
+  end
+
+  def login
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Drpicasa::Application.routes.draw do
   
   match "/auth/google_oauth2/callback", to: "sessions#albums"
+  get "sessions/photos", to:"sessions#photos", :as => :show_photos
   
   root :to => 'sessions#login'
 
