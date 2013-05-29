cc2069b5959e50f6033d12d313d9d005252c1619
kishiamy <kishiamy84@gmail.com>
2013-05-30 01:45:47 +0200

    refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,17 @@
 class SessionsController < ApplicationController
 
-  #TODO: separate get token,get albums and get photos in different methods
-  def albums
-    token = request.env["omniauth.auth"].credentials.token
-    request_xml = RestClient.get("https://picasaweb.google.com/data/feed/api/user/default", 
-                                  headers = { Gdata: 2,
-                                              Authorization: "OAuth #{token}" })
-    session[:params] = token
+  before_filter :get_token, :get_information
 
-    albums_xml = Nokogiri::XML(request_xml) do |config|
-      config.strict.nonet
-    end
-    
-    @albums = albums_xml.xpath('//xmlns:entry').map do |entry|
+  def albums
+    @albums = @albums_xml.xpath('//xmlns:entry').map do |entry|
       {
         :title => entry.xpath('.//media:title').inner_text,
         :thumb => entry.xpath('.//media:thumbnail').attr("url")
       }
     end
- 
+  end
+
+  def photos
     @photos = albums_xml.xpath('//media:group')[0..2].each do |entry|
       {
         :thumb => entry.xpath('.//media:thumbnail').attr("url")
@@ -26,10 +19,23 @@ class SessionsController < ApplicationController
     end
   end
 
-  def photos
+  def login
   end
 
-  def login
+  private
+
+  def get_token
+    @token = request.env["omniauth.auth"].credentials.token
+  end
+
+  def get_information
+    @request_xml = RestClient.get("https://picasaweb.google.com/data/feed/api/user/default", 
+                                  headers = { Gdata: 2,
+                                              Authorization: "OAuth #{@token}" })
+    @albums_xml = Nokogiri::XML(@request_xml) do |config|
+      config.strict.nonet
+    end
+    session[:params] = @token
   end
 
 end
