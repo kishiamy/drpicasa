33a9df5f43649ba26662802c43635c9324d964f4
kishiamy <kishiamy84@gmail.com>
2013-06-01 21:40:48 +0200

    add no photo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,21 +13,31 @@ class SessionsController < ApplicationController
     end
 
     session[:params] = @token
-    
+
     @albums = @albums_xml.xpath('//xmlns:entry').map do |entry|
       {
         :title => entry.xpath('.//media:title').inner_text,
         :thumb => entry.xpath('.//media:thumbnail').attr("url")
+        #@userid => entry.xpath
       }
     end
   end
 
   def photos
-    @photos = @albums_xml.xpath('//media:group')[0..2].each do |entry|
-      {
-        :thumb => entry.xpath('.//media:thumbnail').attr("url")
-      }
-    end
+   
+    # @request_xml = RestClient.get("https://picasaweb.google.com/data/feed/api/user/userID/albumid/albumID", 
+    #                               headers = { Gdata: 2,
+    #                                           Authorization: "OAuth #{session[:params]}" })
+    # @albums_xml = Nokogiri::XML(@request_xml) do |config|
+    #   config.strict.nonet
+    # end
+
+    # @photos = @albums_xml.xpath('//xmlns:entry').map do |entry|
+    # # @photos = @albums_xml.xpath('//media:group')[0..2].map do |entry|
+    #    {
+    #      :thumb => entry.xpath('.//media:thumbnail').attr("url")
+    #    }
+    # end
   end
 
   def login
