228707049608048c76e9035636145e2ae1e902d3
kishiamy <kishiamy84@gmail.com>
2013-06-09 16:51:52 +0200

    configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202205255) do
+ActiveRecord::Schema.define(:version => 20130602183720) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "title"
+    t.integer  "albumID"
+    t.string   "image_thumb"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "identities", :force => true do |t|
     t.string   "email"
     t.string   "password_confirmation"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
+  end
+
+  create_table "photos", :force => true do |t|
+    t.string   "image_path"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
